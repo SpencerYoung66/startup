41b2cfda30833212179ee8a748430dd65b948117
Spencer Young <spenceryoung127@gmail.com>
2023-12-13 18:02:02 -0700

    Added Home

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export function Home() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>home displayed here</div>
+    <body>
+    <main>
+        <h1>Welcome to this year's Labor Day Ice Cream Competition</h1>
+        <div>
+            {/* <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Ice_cream_with_whipped_cream%2C_chocolate_syrup%2C_and_a_wafer_%28cropped%29.jpg" width="300" height="400"> */}
+        </div>
+        <div>
+            <span id="numFlavors">Number of Flavors: 0</span>
+        </div>
     </main>
+    </body>
   );
 }
